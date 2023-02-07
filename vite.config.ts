c791915c23ea29b95348b70bf7ae821698e0d719
Yegor Levandovskyi <yegor.levandovskyi@eneba.com>
2023-02-07 22:20:42 +0200

    Update build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/model-viewer/"
-})
+  base: "/model-viewer/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "@react-three/fiber": ["@react-three/fiber"],
+          "@react-three/drei": ["@react-three/drei"],
+          "@react-three/xr": ["@react-three/xr"],
+          three: ["three"],
+        },
+      },
+    },
+  },
+});
